162d97982353d738244d9cf3015ff10eeaf6f4c4
Omar <omar.sallam@gmail.com>
2022-07-22 15:01:37 +0000

    Added flatten function

---
+++ flatten.js
@@ -0,0 +1,41 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+    
+  }
+}
+
+function flatten(array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let currentArray = array[i];
+    if (!Array.isArray(currentArray)) {
+        flattenedArray.push(currentArray);
+        continue;
+    }
+    for (let j = 0; j < currentArray.length; j++) {
+      flattenedArray.push(currentArray[j])
+    }
+  }
+  return flattenedArray
+}
+
+// Testing
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
