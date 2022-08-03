b30e43d5234df5fd2515a3a0bd48df53d5d43a7d
Omar <omar.sallam@gmail.com>
2022-08-03 19:25:35 -0400

    Adding takeUntil function with Assertion testing

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+
+const takeUntil = function(array, callback) {
+const results = [];
+  for (let item of array) {
+      if (!callback(item)) {
+        results.push(item);
+      } else if (callback(item)) {
+      return results;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+    
+  }
+  console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+assertArraysEqual(results1, ["I've", "been", "to", "Hollywood"]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
