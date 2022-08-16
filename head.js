d8192bfebc00373163b9e25985ae202b812ac460
Omar <omar.sallam@gmail.com>
2022-08-16 15:58:05 -0400

    Incorporating mocha & chai into headTest.js

--- head.js
+++ head.js
@@ -4,4 +4,4 @@ const head = function(actual) {
   return actual[0];
 };
 
-
+module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,13 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = function(actual) {
-  return actual[0];
-}
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Omar");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
